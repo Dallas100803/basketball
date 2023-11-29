7c55368a3cc37ba6e1e38c517686d07280e18aae
Dallas Sims <simsdall@msu.edu>
2023-11-28 20:31:22 -0500

    11-28

---
+++ scripts/scripts.js
@@ -0,0 +1,14 @@
+/*
+Student Name: Dallas Sims
+File Name:script.js
+Date: 11/26/2023
+*/
+
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+        } else {
+            menu.style.display = "block";
+        }
+}
